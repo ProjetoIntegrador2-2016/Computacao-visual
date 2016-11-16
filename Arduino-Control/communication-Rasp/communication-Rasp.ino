b3fce381f39e5a39460557a1ae522c7d96ed09bd
DaniloBarros <dan.b412@gmail.com>
2016-11-16 07:30:02 -0200

    Changed arduino algorithm to implement control theory

--- Arduino-Control/communication-Rasp/communication-Rasp.ino
+++ Arduino-Control/communication-Rasp/communication-Rasp.ino
@@ -1,4 +1,5 @@
 #include <NewPing.h>
+#include <math.h> 
 
 // Clockwise and counter-clockwise definitions.
 // Depending on how you wired your motors, you may need to swap.
@@ -17,13 +18,21 @@
 #define TURN_LEFT 'e'
 #define TURN_RIGHT 'd'
 
+#define DISTANCE_MOTOR_CENTER  7
+#define WHEEL_RADIUS 3
+
 const byte PWMA = 3;  // PWM control (speed) for motor A
 const byte PWMB = 11; // PWM control (speed) for motor B
 const byte DIRA = 12; // Direction control for motor A
 const byte DIRB = 13; // Direction control for motor B
 
-char incoming = 'n';
+char incoming = 'i';
 int last_cw = CW;
+double error_sum = 0;
+double old_error = 0;
+double Kp = 0.5;
+double Ki = 0.025;
+double old_phi = PI/2; // Start looking foward
 
 //Determina o numero de sensores no circuito
 #define SONAR_NUM     3
@@ -55,7 +64,7 @@ NewPing sonar[SONAR_NUM] =
 
 void setup()
 {
-  Serial.begin(9600);
+  Serial.begin(19200);
   setupArdumoto();
 
  //Inicia a primeira medicao com 75ms
@@ -74,9 +83,11 @@ void loop()
     if (millis() >= pingTimer[i]) {         
       pingTimer[i] += PING_INTERVAL * SONAR_NUM;  
       
+      /*
       if (i == 0 && currentSensor == SONAR_NUM - 1){
         oneSensorCycle();
       }
+      */
       
       sonar[currentSensor].timer_stop();
       currentSensor = i;
@@ -84,106 +95,43 @@ void loop()
       sonar[currentSensor].ping_timer(echoCheck);
     }
   }
-  
+
+  String buffer = "";
   if (Serial.available()){
-    
-    incoming = Serial.read();
-    switch(incoming){
-      case FOWARD:
-        if(cm[0] > MIN_DISTANCE){
-          driveArdumoto(MOTOR_A, CW, 100);
-          driveArdumoto(MOTOR_B, CW, 100);
-          last_cw = CW;  
-          
-        } else {
-          stopArdumoto(MOTOR_A);
-          stopArdumoto(MOTOR_B);
-        }
-      break;
-      case BACKWARD:
-        driveArdumoto(MOTOR_A, CCW, 100);
-        driveArdumoto(MOTOR_B, CCW, 100);
-        last_cw = CCW;
-      break;
-      case RIGHT:
-        if(cm[1] > MIN_DISTANCE) {
-          if (last_cw == CW) {
-            analogWrite(PWMA, 50);
-            analogWrite(PWMB, 200);
-          } else {
-            analogWrite(PWMA, 200);
-            analogWrite(PWMB, 50);
-          }
-        } else if(cm[2] > MIN_DISTANCE) { // There is no object in left side
-          // Continue last direction
-          driveArdumoto(MOTOR_A, last_cw, 100);
-          driveArdumoto(MOTOR_B, last_cw, 100);
-          delay(2);
-          
-          // Go Right
-          if (last_cw == CW) {
-            analogWrite(PWMA, 50);
-            analogWrite(PWMB, 200);
-          } else {
-            analogWrite(PWMA, 200);
-            analogWrite(PWMB, 50);
-          }
-        }
-        
-      break;
-      case LEFT:
-        if(cm[2] > MIN_DISTANCE) {
-          if (last_cw == CW){ 
-            analogWrite(PWMA, 200);
-            analogWrite(PWMB, 50);
-          } else{
-            analogWrite(PWMA, 50);
-            analogWrite(PWMB, 200);
-          }
-        } else if(cm[1] > MIN_DISTANCE) {
-          // Continue last direction
-          driveArdumoto(MOTOR_A, last_cw, 100);
-          driveArdumoto(MOTOR_B, last_cw, 100);
-          delay(2);
-          
-          if (last_cw == CW){ 
-            analogWrite(PWMA, 200);
-            analogWrite(PWMB, 50);
-          } else{
-            analogWrite(PWMA, 50);
-            analogWrite(PWMB, 200);
-          }
-        }
-      break;
-      case TURN_RIGHT:
-         if(cm[1] > MIN_DISTANCE) {
-           stopArdumoto(MOTOR_A);
-           stopArdumoto(MOTOR_B);
-           driveArdumoto(MOTOR_A, CCW, 200);
-         } else {
-           driveArdumoto(MOTOR_A, CW, 100);
-           driveArdumoto(MOTOR_B, CW, 100);
-           delay(5);
-         }
-      break;
-      case TURN_LEFT:
-        if(cm[2] > MIN_DISTANCE) {
-           stopArdumoto(MOTOR_A);
-           stopArdumoto(MOTOR_B);
-           driveArdumoto(MOTOR_B, CCW, 200);
-        } else {
-           driveArdumoto(MOTOR_A, CW, 100);
-           driveArdumoto(MOTOR_B, CW, 100);
-           delay(5);          
-        }
-      break;
-      default:
-        stopArdumoto(MOTOR_A);
-        stopArdumoto(MOTOR_B);
+    while(Serial.available()){
+      buffer += Serial.read();
     }
-    delay(1);
-    
   }
+
+  double pos[2] = {20, 0.01}; // x and y position of the target
+  if(buffer.length() > 1){
+    for(int i = 0; i < 2; i++){
+      int index = buffer.indexOf(","); // Find the next comma
+      pos[i] = atol(buffer.substring(0, index).c_str()); // Extract the number
+      buffer = buffer.substring(index+1); // Exclude the number read
+    }
+  }
+
+  double phi_desired = atan2(pos[0],pos[1]);
+  if (phi_desired < 0){
+    phi_desired += PI;
+  }
+
+  // -------------- PID
+  double error = phi_desired - old_phi;
+  //double error_dot = old_error; Used for Kd
+  error_sum = error_sum + error;
+
+  double gain = Kp * error + Ki * error_sum;
+  //old_error = error;
+  // -------------- END PID
+
+  double velocity = pos[0]/cos(phi_desired);
+
+  Serial.print(gain);
+  Serial.println();
+  
+  old_phi = phi_desired;
 }
 
 // driveArdumoto drives 'motor' in 'dir' direction at 'spd' speed
