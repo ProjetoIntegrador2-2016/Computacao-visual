50bba73ad79aa15cb828bcbad277be27a8b0853b
Tainara Reis <tainara.s.r@hotmail.com>
2016-12-07 18:05:30 -0200

    Functions to avoid obstacles via ultrassonic sensors

--- Arduino-Control/communication-Rasp/communication-Rasp.ino
+++ Arduino-Control/communication-Rasp/communication-Rasp.ino
@@ -14,6 +14,13 @@
 #define MAX_DISTANCE  50 // Distancia de deteccao
 #define MIN_DISTANCE  20
 #define PING_INTERVAL 33 //Intervalo entre as medicoes - valor minimo 29ms
+#define RIGHT "RIGHT"
+#define LEFT "LEFT"
+#define FOWARD "FOWARD"
+#define FOWARD_SENSOR 0
+#define RIGHT_SENSOR 1
+#define LEFT_SENSOR 2
+#define ARBITRARY 10 //valor "mínimo" pra julgar se o target está a RIGHT ou LEFT
 
 // Pin definitions for Ultrasonic sensors
 const byte TRIGGER_F = 25;
@@ -42,7 +49,7 @@ unsigned int cm[SONAR_NUM]; //Armazena o numero de medicoes
 uint8_t currentSensor = 0; // Armazena o numero do sensor ativo
 
 // Assign Ultrassonic sensors to pins
-NewPing sonar[SONAR_NUM] = { 
+NewPing sonar[SONAR_NUM] = {
   NewPing(TRIGGER_F, ECHO_F, MAX_DISTANCE),
   NewPing(TRIGGER_R, ECHO_R, MAX_DISTANCE),
   NewPing(TRIGGER_L, ECHO_L, MAX_DISTANCE),
@@ -54,49 +61,49 @@ void setup(){
   Serial.begin(9600);
   Serial1.begin(9600);
   setupArdumoto();
-  
+
  //Inicia a primeira medicao com 75ms
   pingTimer[0] = millis() + 75;
   //Define o tempo de inicializacao de cada sensor
   for (uint8_t i = 1; i < SONAR_NUM; i++){
     pingTimer[i] = pingTimer[i - 1] + PING_INTERVAL;
   }
-    
+
   lastTime = millis();
 
   Serial.print("Ready to roll!");
-  
+
 }
 
 // ################################## LOOP ########################################
 
 void loop(){
-  
+
   loopSensors();
-  
+
   float sampleXY[2];
-  
+
   if (Serial1.available() > 0){
     // Read from serial
     sampleXY[0] = Serial1.parseFloat();
     sampleXY[1] = Serial1.parseFloat();
     Serial1.flush();
-    
+
     vector sample = checkValues(sampleXY[0], sampleXY[1]);
     //vector sample(sampleXY[0], sampleXY[1]);
-    
-    
+
+
     // Calculate PID gains
     double omega = angularPID.addNewSample(sample);
     //double distanceGain = distancePID.addNewSample(sample.calculateMagnitude());
-    
+
     //angularPID.addNewSample(sample);
-    
+
     Serial.print("Omega: ");
     Serial.print(angularPID.process());
     Serial.println();
     goToPosition(sample);
-    
+
 //    int velocityLeftWheel = 0;
 //    int velocityRightWheel = 0;
 //    calculateWheelsVelocities(sample, velocityLeftWheel, velocityRightWheel);
@@ -104,11 +111,11 @@ void loop(){
 //    Serial.print(velocityLeftWheel);
 //    Serial.print("~");
 //    Serial.print(velocityRightWheel);
-//    
+//
 //    int dirA = 0;
 //    int dirB = 0;
 //    computePwmAndDirections(dirA, dirB, velocityRightWheel, velocityLeftWheel);
-//    
+//
 //    Serial.print("\nPWM: ");
 //    Serial.print(velocityLeftWheel);
 //    Serial.print("~");
@@ -120,10 +127,10 @@ void loop(){
       Serial.print(sampleXY[0]);Serial.print("--");Serial.print(sampleXY[1]);
       Serial.print("\n\n");
     }
-    
+
     oldSample = sample;
   }
-  
+
   Serial1.flush();
   Serial.flush();
 }
@@ -136,66 +143,66 @@ vector checkValues(float xPos, float yPos){
   if (yPos >= 0) {
     // Looks good, no invalid token
     result = filterDiscrepancies(xPos, yPos);
-  
+
   } else { // Target not found, go to last known position then search it
-    
+
     // If there is a last known position, go to it
-    if (oldSample.calculateMagnitude() != 0){ 
+    if (oldSample.calculateMagnitude() != 0){
       goToPosition(oldSample);
       delay(500); // Wait 0.5 s for target to appear
-      
+
     } else { // Search target
       int dirA = CW;
       int dirB = CW;
-      
+
       if (xPos < 0){ // Last position is to the left
         dirB = CCW;
       } else { // Last position to the right, spin right
         dirA = CCW;
       }
-      
+
       driveArdumoto(MOTOR_A, dirA, 100);
       driveArdumoto(MOTOR_B, dirB, 100);
-  
+
       // Spin for 0.05 s then look for target
-      delay(50); 
+      delay(50);
       stopRobot();
-      
+
       // Clear samples as it were in the begining
       result = vector();
       oldSample = result;
       angularPID.reset();
     }
   }
-  
+
   return result;
 }
 
 // Seamless to a low pass filter
 vector filterDiscrepancies(float xPos, float yPos){
   vector result(xPos, yPos);
-  
+
   float oldX = oldSample.xPos;
   float oldY = oldSample.yPos;
 
   const int errorMargin = 8;
-  
+
   // Check if new Pos is between the acceptable erro margin
   // only if the oldSample is bigger than (0, 0)
   if (oldSample.calculateMagnitude() != 0){
-    
+
     if (xPos <= oldX + errorMargin && xPos >= oldX - errorMargin){
       // Looks good, do nothing
     } else {
-      result.xPos = oldX;  
+      result.xPos = oldX;
     }
 
     if (yPos <= oldY + errorMargin && yPos >= oldY - errorMargin){
       // Looks good, do nothing
     } else {
-      result.yPos = oldY;  
+      result.yPos = oldY;
     }
-    
+
   } else {
     // Do nothing
   }
@@ -206,21 +213,21 @@ vector filterDiscrepancies(float xPos, float yPos){
 
 void goToPosition(vector targetPosition){
   vector nullVector(0.0, 0.0);
-  
+
   if (targetPosition != nullVector){
     int velocityLeftWheel = 0;
     int velocityRightWheel = 0;
     calculateWheelsVelocities(targetPosition, velocityLeftWheel, velocityRightWheel);
-    
+
     int dirA = 0;
     int dirB = 0;
     computePwmAndDirections(dirA, dirB, velocityRightWheel, velocityLeftWheel);
-  
+
     driveArdumoto(MOTOR_A, dirA, velocityRightWheel);
     driveArdumoto(MOTOR_B, dirB, velocityLeftWheel);
-  
+
   } else {
-    stopRobot();  
+    stopRobot();
   }
 }
 
@@ -228,7 +235,7 @@ void goToPosition(vector targetPosition){
 void calculateWheelsVelocities(const vector targetPosition, int &leftWheel, int &rightWheel){
   float velocity = calculateVelocity(targetPosition);
   double omega = angularPID.process();
-  
+
   leftWheel = (2 * velocity + omega * DISTANCE_MOTOR_CENTER) / (2 * WHEEL_RADIUS);
   rightWheel = (2 * velocity - omega * DISTANCE_MOTOR_CENTER) / (2 * WHEEL_RADIUS);
 
@@ -241,7 +248,7 @@ void calculateWheelsVelocities(const vector targetPosition, int &leftWheel, int 
           Serial.print(velocity);Serial.print(" + ");Serial.print(omega);
           Serial.print(" * 7) / ( 2 * 3)\n");
   }
-  
+
 }
 
 
@@ -252,25 +259,25 @@ void computePwmAndDirections(int &dirA, int &dirB, int &pwmA, int &pwmB){
   if (pwmA > 0){ // Go foward
     pwmA += pwmGainConstant;
     dirA = CW;
-  
+
   } else if (pwmA < 0){ // Go backward
     pwmA -= pwmGainConstant;
     pwmA = abs(pwmA);
     dirA = CCW;
-  
+
   } else { // Stay put
     pwmA = 0;
   }
-  
+
   if (pwmB > 0){
     pwmB += pwmGainConstant;
     dirB = CW;
-  
+
   } else if (pwmB < 0){
     pwmB -= pwmGainConstant;
     pwmB = abs(pwmB);
     dirB = CCW;
-  
+
   } else {
     pwmB = 0;
   }
@@ -284,11 +291,87 @@ void computePwmAndDirections(int &dirA, int &dirB, int &pwmA, int &pwmB){
 
 // ############################ ULTRASONIC METHODS ################################
 
+targetDirection = FOWARD;
+
+void goToPosition(vector targetPosition){
+  vector nullVector(0.0, 0.0);
+
+  if (targetPosition != nullVector){
+    detectObstacles(targetPosition);
+    moveCart(targetPosition);
+  } else {
+    stopRobot();
+  }
+}
+
+void detectObstacles(targetPosition){
+  loopSensors();
+  targetDirection = detectDirection(targetPosition);
+  if (targetDirection == RIGHT){
+    if (MIN_DISTANCE => cm[RIGHT_SENSOR]){
+      deflectObstacleStatically(RIGHT);
+    }
+  } else if (targetDirection == LEFT){
+    if (MIN_DISTANCE => cm[LEFT_SENSOR]){
+      deflectObstacleStatically(LEFT);
+  } else if (targetPosition == FOWARD){
+    if (MIN_DISTANCE => cm[FOWARD_SENSOR]){
+      //stopRobot()
+    }
+  }
+}
+
+void detectDirection(targetPosition){
+  double omega = angularPID.process();
+  if (omega > ARBITRARY){
+    targetDirection = RIGHT;
+  } else if (omega < -ARBITRARY){
+    targetDirection = LEFT;
+  } else {
+    targetDirection = FOWARD;
+  }
+  return targetDirection;
+}
+
+
+void deflectObstacleStatically(obstacleDirection){
+  if (obstacleDirection == RIGHT){
+    driveArdumoto(MOTOR_A, CW, velocityRightWheel); //setar o velocityRightWheel
+    driveArdumoto(MOTOR_B, CW, velocityLeftWheel); //setar o velocityLeftWheel
+  } else if (obstacleDirection == LEFT){
+    driveArdumoto(MOTOR_A, CW, velocityRightWheel); //setar o velocityRightWheel
+    driveArdumoto(MOTOR_B, CW, velocityLeftWheel); //setar o velocityLeftWheel
+  } else { //obstáculo a frente
+    //stopRobot();
+  }
+}
+
+
+
+void moveCart(vector targetPosition){
+  int velocityLeftWheel = 0;
+  int velocityRightWheel = 0;
+  calculateWheelsVelocities(targetPosition, velocityLeftWheel, velocityRightWheel);
+
+  int dirA = 0;
+  int dirB = 0;
+  computePwmAndDirections(dirA, dirB, velocityRightWheel, velocityLeftWheel);
+
+  driveArdumoto(MOTOR_A, dirA, velocityRightWheel);
+  driveArdumoto(MOTOR_B, dirB, velocityLeftWheel);
+}
+
+//
+void deflectObstacleDinamically(targetPosition){
+  moveCart(targetPosition);
+}
+
+
 void loopSensors(){
   for (uint8_t i = 0; i < SONAR_NUM; i++) {
-    if (millis() >= pingTimer[i]) {         
-      pingTimer[i] += PING_INTERVAL * SONAR_NUM;  
-     
+    if (millis() >= pingTimer[i]) {
+      pingTimer[i] += PING_INTERVAL * SONAR_NUM;
+
       if (i == 0 && currentSensor == SONAR_NUM - 1){
         oneSensorCycle();
       }
@@ -303,44 +386,22 @@ void loopSensors(){
 
 
 //Se receber um sinal (eco), calcula a distancia
-void echoCheck() { 
+void echoCheck() {
   if (sonar[currentSensor].check_timer()) {
     cm[currentSensor] = sonar[currentSensor].ping_result / US_ROUNDTRIP_CM;
   }
 }
 
 
-void oneSensorCycle() { 
+void oneSensorCycle() {
   // Ciclo de leitura do sensor
   for (uint8_t i = 0; i < SONAR_NUM; i++) {
     //Imprime os valores lidos pelos sensores, no serial monitor
     Serial.print("Sensor : ");
-    Serial.print(i); 
+    Serial.print(i);
     Serial.print(" = ");
     Serial.print(cm[i]);
     Serial.print(" cm - ");
   }
   Serial.println();
 }
-
-
-void differentialDrive() {
-    //When the two wheels are spinning in the same direction 
-    //and the same speed, the robot will move straight.
-    
-    //driveArdumoto(MOTOR_A, )
-    //driveArdumoto(MOTOR_B, )
-    
-    //When the two wheels are spinning in the same direction, 
-    //but with different speeds, the robot will turn away from 
-    //the faster motor. For example, if the right wheel is spinning 
-    //faster than the left, the motor will turn left.
-    
-    
-    //If the two wheels are spinning with the same speed, 
-    //but in opposite directions, the robot will rotate in place, 
-    //spinning around the midpoint between the two wheels.
-}
-
-
-
